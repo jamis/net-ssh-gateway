29485b8ccf7c6a9b9afa9d2d7f41b8ec0875be0f
delano <delano@solutious.com>
2011-04-30 20:42:11 -0400

    Release 1.1.0

--- lib/net/ssh/gateway.rb
+++ lib/net/ssh/gateway.rb
@@ -40,10 +40,10 @@ class Net::SSH::Gateway
     MAJOR = 1
 
     # The minor component of the library's version
-    MINOR = 0
+    MINOR = 1
 
     # The tiny component of the library's version
-    TINY  = 1
+    TINY  = 0
 
     # The library's version as a Version instance
     CURRENT = new(MAJOR, MINOR, TINY)
