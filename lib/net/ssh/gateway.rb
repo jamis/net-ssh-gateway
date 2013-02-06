04db93e5c06af7fa48752f7cb908acbe305ad48c
delano <delano.mandelbaum@gmail.com>
2013-02-06 11:03:02 -0800

    Updated net-ssh dep to 2.6.5

--- lib/net/ssh/gateway.rb
+++ lib/net/ssh/gateway.rb
@@ -43,7 +43,7 @@ class Net::SSH::Gateway
     MINOR = 1
 
     # The tiny component of the library's version
-    TINY  = 1
+    TINY  = 2
 
     # The library's version as a Version instance
     CURRENT = new(MAJOR, MINOR, TINY)
